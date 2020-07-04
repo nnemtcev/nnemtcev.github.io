9b208fccf2ec0802a6d131471fb83a3c2ec79356
Nikita Nemtcev <nemtcevn@gmail.com>
2020-07-03 20:44:16 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Typed from 'typed.js';
+
+const typed = new Typed('#heading-primary-sub', {
+  strings: [
+    'is a front-end engineer',
+    'is a student at UWO',
+    'is looking for an internship',
+  ],
+  typeSpeed: 75,
+  backSpeed: 100,
+  startDelay: 1200,
+});
